29416bf5366a8cefee6aa084eed1f4fc602c656e
Ernis <e.badmaev@gmail.com>
2022-09-16 10:03:14 +0300

    feat: add css loader and rules in buildLoaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,10 +1,22 @@
 import webpack from 'webpack';
 
+const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+    ],
+};
+
 export function buildLoaders(): webpack.RuleSetRule[] {
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     };
-    return [typescriptLoader];
+    return [typescriptLoader, cssLoader];
 }
