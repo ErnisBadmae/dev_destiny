2ba158b2558b5f109ae8849698966c2e2f01031d
Ernis <e.badmaev@gmail.com>
2022-09-19 09:39:58 +0300

    feat: themeContext and themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
+import { Suspense, useState } from 'react';
 
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
-import './index.scss';
-import { Suspense } from 'react';
+import './styles/index.scss';
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
 
 function App() {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    };
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>переключить тему</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div> Loading ...</div>}>
