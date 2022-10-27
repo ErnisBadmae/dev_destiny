4d00b0d528f100d4a7b3468fe2f4afd7324ac96a
Ernis <e.badmaev@gmail.com>
2022-10-27 12:31:51 +0800

    not working i18n plugin

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,16 +2,15 @@
 import { useTranslation } from 'react-i18next';
 
 function MainPage() {
-    
-    //не отрисовывает эти компоненты
-    //
+
     const {t} = useTranslation()
 
 
     return (
-    <div style={{background:"red"}}>
-       {t('Главная страница')}   
-    </div>)
+      <div>
+        {t("Главная страница")}
+      </div>
+    );
 }
 
 export default MainPage;
