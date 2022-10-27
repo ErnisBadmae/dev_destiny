666f8a20965fca86b63b7c64680808babe1fe053
Ernis <e.badmaev@gmail.com>
2022-10-27 12:38:13 +0800

    install refresh plugin webpacj

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,6 +17,7 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
            __IS_DEV__: JSON.stringify(isDev)
-          })
+          }),
+        new webpack.HotModuleReplacementPlugin(),
     ];
 }
