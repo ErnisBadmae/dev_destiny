8e0cd49669f85aa087a9309321cbfcb1b1affa4a
Ernis <e.badmaev@gmail.com>
2022-11-08 21:43:04 +0800

    fix: dev plugins bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -10,7 +10,8 @@ BundleAnalyzerPlugin
 export function buildPlugins({
     paths,isDev
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -22,9 +23,16 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer:false
-        })
-    ];
+      
+    ]
+    if(isDev) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+            new BundleAnalyzerPlugin({
+                openAnalyzer:false
+            }))
+    }
+    return plugins
+
+    
 }
