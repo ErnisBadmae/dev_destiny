6d7d795abbf212d0ca09b4b6925931452294c7f7
Ernis <e.badmaev@gmail.com>
2022-11-18 13:42:32 +0800

    try: github action

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-// import { useTheme } from 'app/providers/ThemeProvider';
+import { useTheme } from 'app/providers/ThemeProvider';
 import React, { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import {classNames} from 'shared/lib/className/className';
 import { Portal } from '../Portal/Portal';
@@ -24,11 +24,7 @@ export const Modal=(props: ModalProps)=>  {
 
     const [isClosing, setIsClosing] = useState(false)
     const timeRef = useRef<ReturnType<typeof setTimeout>>()
-     
-    // const [theme] = useTheme()
-
  
-
     const closeHandler = useCallback(() => {
         if(onClose) {
             setIsClosing(true)
