97063b40f21a7b46d0fee55e8d02202babd364af
Ernis <e.badmaev@gmail.com>
2023-02-12 14:42:09 +0300

    feat: create axios interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,11 +2,14 @@ import { USER_LOCALSTORAGE_KEY } from './../const/localStorage';
 import axios from "axios"
 
 
-
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
+})
+
+$api.interceptors.request.use((config) => {
+    if(config.headers) {
+
+        config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
     }
-    
+    return config
 })
